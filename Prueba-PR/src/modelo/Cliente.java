7ffdd0ac110504f85cda1e34fd39b2d67003bc57
zu2220 <u22206853@utp.edu.pe>
2025-05-09 19:33:17 -0500

    Se está subiendo el proyecto con el paquete modelo que por el momento solo contiene dos clases: Cliente y PDF.

---
+++ Prueba-PR/src/modelo/Cliente.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author oscar
+ */
+public class Cliente {
+    private String id;
+    private String nombre;
+    private String dni;
+
+    public Cliente() {
+    }
+
+    public Cliente(String id, String nombre, String dni) {
+        this.id = id;
+        this.nombre = nombre;
+        this.dni = dni;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+    
+}
